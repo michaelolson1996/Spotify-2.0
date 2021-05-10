77d0fd59e0895edbec0c3649d0c4a1f2eaaa06b6
michaelolson1996 <michaelolson.1996@protonmail.com>
2021-05-09 18:45:36 -0600

    added DotNetEnv and Newtonsoft libraries, set up retrieve token function in Backend, pull variables from .env file and .gitignore file to ignore .env file

--- Backend/Backend.cs
+++ Backend/Backend.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using DotNetEnv;
+using System.Net.Http;
+using System.Text.Json;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace Spotify_2._0.Backend
 {
     class Backend
     {
+        private string ClientId;
+        private string ClientSecret;
+
+        class AccessToken
+        {
+            public string access_token { get; set; }
+            public string token_type { get; set; }
+            public long expires_in { get; set; }
+        }
+
+        public Backend()
+        {
+            InitializeEnvironment();
+            
+        }
+
+        private void InitializeEnvironment()
+        {
+            Env.TraversePath().Load();
+            ClientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+            ClientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+
+            _ = RetrieveToken();
+        }
+
+        private async Task RetrieveToken()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // add client id and client secret to the authorization header
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                        "Basic",
+                        Convert.ToBase64String(Encoding.UTF8.GetBytes($"{ClientId}:{ClientSecret}")));
+
+                // append the grant type to the request body
+                List<KeyValuePair<string, string>> requestData = new List<KeyValuePair<string, string>>();
+                requestData.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+
+                FormUrlEncodedContent requestBody = new FormUrlEncodedContent(requestData);
+
+                // send request and await response
+                var request = await client.PostAsync("https://accounts.spotify.com/api/token", requestBody);
+                var response = await request.Content.ReadAsStringAsync();
+
+                JsonSerializer.
+
+                return JsonConvert.DeserializeObject<AccessToken>(response);
+            }
+        }
     }
 }
