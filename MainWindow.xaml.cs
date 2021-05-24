5102114250e893ba70523f3083f58159f4829fba
Nicholas Roberson <60959101+lawlietl4@users.noreply.github.com>
2021-05-24 08:28:06 -0600

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,6 +37,7 @@ namespace Spotify_2._0
         /// <param name="search"></param>
         public async void GetBackendInfo(string search)
         {
+            Playlist_Text_Block.Children.Clear();
             List<Playlist> playlists = await backend2.GetPlaylists(search);
 
             playlists.ForEach(playlist => {
@@ -50,8 +51,8 @@ namespace Spotify_2._0
         /// <summary>
         /// returns the song information and playlist data from the backend
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">WPF handling how buttons send actions</param>
+        /// <param name="e">arguments that some buttons and actions require</param>
         private async void SongsReturn(object sender, RoutedEventArgs e)
         {
             Song_Text_Block.Children.Clear();
@@ -66,10 +67,10 @@ namespace Spotify_2._0
                 string retStr = "";
                 song.Artists.ForEach(artist =>
                 {
-                    retStr += artist.Name;
+                    retStr += artist.Name + '\n';
                 });
                 btn.Tag = song.preview_url;
-                btn.DataContext += $"Artist: {retStr}\n";
+                btn.DataContext += $"Artist(s): {retStr}\n";
                 btn.DataContext += $"Album: {song.album_name}";
                 btn.MouseDoubleClick += Btn_MouseDoubleClick;
                 btn.Click += new RoutedEventHandler(ArtistAlbumReturn);
